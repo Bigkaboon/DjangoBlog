f3d358c5246beea490eea75deaff27f38d837cb3
Bigkaboon <>
2023-12-06 08:07:52 +0000

    update

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Bigkaboon.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-bigkaboon-djangoblog-xkkwjcsvig.us2.codeanyapp.com', 'Bigkaboon.herokuapp.com', 'localhost']
 
 
 # Application definition
